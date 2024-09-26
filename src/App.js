7da84b1ad1dca7b38fb4a131df98fcc5ca2094be
Giggles3237 <chris.lasko@gmail.com>
2024-09-26 07:36:31 -0400

    Prepare frontend for deployment

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import SalesTable from './components/SalesTable';
 import EditSaleForm from './components/EditSaleForm';
 import DateRangePicker from './components/DateRangePicker';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api';
 
 function App() {
   const [sales, setSales] = useState([]);
